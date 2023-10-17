a6c590b9d1d41e9456bfadca3e0365f82aac6806
DevRomu <orinarhomill@gmail.com>
2023-10-17 16:38:57 -0500

    Changed some arrangements

--- app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
@@ -15,12 +15,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.materialdesigndemo.ui.theme.MaterialDesignDemoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
