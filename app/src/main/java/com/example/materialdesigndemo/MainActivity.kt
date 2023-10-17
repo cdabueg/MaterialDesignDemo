05ce48857eac25b53ec686af414781f3b5570313
DevRomu <orinarhomill@gmail.com>
2023-10-17 16:29:39 -0500

    Changes made

--- app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.materialdesigndemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.materialdesigndemo.ui.theme.MaterialDesignDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +29,93 @@ class MainActivity : ComponentActivity() {
             MaterialDesignDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    )
+                    {
+                        // Centered profile icon
+                        ProfileIcon(imageVector = Icons.Default.Person, modifier = Modifier
+                            .fillMaxSize()
+                            .padding(15.dp), alignment = Alignment.Center)
+                    }
+                    ContactForm()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun ProfileIcon(imageVector: ImageVector, modifier: Modifier, alignment: Alignment) {
+    Box(
+        modifier = modifier,
+        contentAlignment = alignment
+    ) {
+        Icon(imageVector = imageVector, contentDescription = "Profile Icon", modifier = Modifier.size(50.dp))
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    MaterialDesignDemoTheme {
-        Greeting("Android")
+fun ContactForm() {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var message by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "Name") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        TextField(
+            value = message,
+            onValueChange = { message = it },
+            label = { Text("Message") },
+            maxLines = 5,
+            leadingIcon = { Icon(imageVector = Icons.Default.Send, contentDescription = "Message") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    // Handle submission or other actions here
+                }
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                // Handle button click (form submission, etc.)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        ) {
+            Text("Submit")
+        }
     }
 }
