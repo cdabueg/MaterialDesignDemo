890e2cf1a1bf32cf929e43a514b04a7044ae44b9
cabuegrrc <95055521+cabuegrrc@users.noreply.github.com>
2023-10-17 15:15:27 -0500

    Stable Layout

--- app/src/main/java/com/example/materialdesigndemo/ContactCard.kt
+++ app/src/main/java/com/example/materialdesigndemo/ContactCard.kt
@@ -1,27 +1,87 @@
+//package com.example.materialdesigndemo
+//
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.material3.*
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import com.example.materialdesigndemo.ui.theme.TestRed
+//
+//
+//@Composable
+//fun ContactCard(
+//    name: String,
+//    email: String,
+//    message: String,
+//    modifier: Modifier = Modifier
+//){
+//    Card(
+//        modifier = modifier,
+//        colors = CardDefaults.cardColors(
+//            containerColor = MaterialTheme.colorScheme.background,
+//        ),
+//        shape = MaterialTheme.shapes.large
+//    ){
+//        Column(
+//            modifier = Modifier.padding(16.dp)
+//        ) {
+//            Text(
+//                text = name,
+//                style = MaterialTheme.typography.bodyLarge,
+//                color = TestRed
+//            )
+//            Spacer(modifier = Modifier.height(8.dp))
+//            Text(
+//                text = email,
+//                style = MaterialTheme.typography.bodyLarge
+//            )
+//            Spacer(modifier = Modifier.height(8.dp))
+//            Text(
+//                text = message,
+//                style = MaterialTheme.typography.bodyLarge
+//            )
+//            Spacer(modifier = Modifier.height(8.dp))
+////            Flowrow(
+////                modifier = Modifier.fillMaxWidth(),
+////                mainAxisSpacing = 8.dp,
+////                // mainAxisSize = SizeMode.Wrap
+////            ){
+////
+////            }
+//        }
+//    }
+//}
+
 package com.example.materialdesigndemo
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.example.materialdesigndemo.ui.theme.TestRed
+import kotlin.random.Random
 
-
+@ExperimentalMaterial3Api
 @Composable
 fun ContactCard(
     name: String,
     email: String,
     message: String,
     modifier: Modifier = Modifier
-){
+) {
     Card(
         modifier = modifier,
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.background,
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
         ),
         shape = MaterialTheme.shapes.large
-    ){
+    ) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
@@ -41,13 +101,6 @@ fun ContactCard(
                 style = MaterialTheme.typography.bodyLarge
             )
             Spacer(modifier = Modifier.height(8.dp))
-//            Flowrow(
-//                modifier = Modifier.fillMaxWidth(),
-//                mainAxisSpacing = 8.dp,
-//                // mainAxisSize = SizeMode.Wrap
-//            ){
-//
-//            }
         }
     }
 }
--- app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
@@ -1,58 +1,121 @@
+//package com.example.materialdesigndemo
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import com.example.materialdesigndemo.ui.theme.MaterialDesignDemoTheme
+//
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MaterialDesignDemoTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background) {
+//                    // Greeting("Android")
+//                    ContactCard(name = "test name", email = "test@email", message = "test message")
+////                    Scaffold {
+////                        LazyColumn {
+////                            items(20){
+////                                ContactCard(name = "test name", email = "test@email", message = "test message")
+////                            }
+////                        }
+////
+////                    }
+//                }
+//
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//            text = "Hello TEST $name!",
+//            modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MaterialDesignDemoTheme {
+//        Greeting("Android")
+//    }
+//}
+//
+
 package com.example.materialdesigndemo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.materialdesigndemo.ui.theme.MaterialDesignDemoTheme
 
+
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialDesignDemoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    // Greeting("Android")
-                    ContactCard(name = "test name", email = "test@email", message = "test message")
-//                    Scaffold {
-//                        LazyColumn {
-//                            items(20){
-//                                ContactCard(name = "test name", email = "test@email", message = "test message")
-//                            }
-//                        }
-//
-//                    }
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = {}) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                            }
+                        },
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(text = "Material 3")
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    ) { values ->
+                        LazyColumn(contentPadding = values) {
+                            items(20) {
+                                ContactCard(
+                                    name = "test name",
+                                    email = "test@email",
+                                    message = "test message")
+                            }
+                        }
+                    }
                 }
-
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello TEST $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaterialDesignDemoTheme {
-        Greeting("Android")
-    }
-}
-
