abb38d1686b4c698d86dacca1d79970ef66bd85b
cabuegrrc <95055521+cabuegrrc@users.noreply.github.com>
2023-10-22 12:37:14 -0500

    Display cards completed

--- app/src/main/java/com/example/materialdesigndemo/ContactCard.kt
+++ app/src/main/java/com/example/materialdesigndemo/ContactCard.kt
@@ -1,10 +1,12 @@
 package com.example.materialdesigndemo
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -19,38 +21,61 @@ fun ContactCard(
 ) {
     ElevatedCard(
         elevation = CardDefaults.cardElevation(
-            defaultElevation = 6.dp
+            defaultElevation = 16.dp
         ),
-        modifier = modifier,
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
         ),
-        shape = MaterialTheme.shapes.medium
+        shape = MaterialTheme.shapes.large,
+        modifier = modifier,
     ) {
         Column(
             modifier = Modifier
-                .padding(18.dp)
                 .fillMaxWidth()
+                .padding(8.dp)
+                .border(
+                    2.dp,
+                    color = MaterialTheme.colorScheme.secondary,
+                    shape = MaterialTheme.shapes.small
+                )
+                .padding(18.dp)
         ) {
             Icon(
                 Icons.Filled.AccountCircle,
-                contentDescription = null
+                contentDescription = null,
+                modifier = Modifier
+                    .align(CenterHorizontally)
+                    .size(48.dp)
+//                    .border(
+//                        1.dp,
+//                        color = MaterialTheme.colorScheme.secondary,
+//                        shape = MaterialTheme.shapes.small
+//                    )
             )
             Text(
                 text = name,
                 style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
             )
             // Space between texts
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = email,
+                text = message,
+                style = MaterialTheme.typography.bodyLarge,
                 textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyLarge
+                modifier = Modifier
+                    .fillMaxWidth()
             )
+
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = message,
-                style = MaterialTheme.typography.bodyLarge
+                text = email,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
             )
             Spacer(modifier = Modifier.height(8.dp))
         }
